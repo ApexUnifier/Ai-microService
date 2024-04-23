dffc57513142d059df70de32c985c8d4ef21f08e
Hemant Singh <Hemantsingh09103@gmail.com>
2024-04-23 17:06:30 +0530

    basic testing complete

--- src/Controllers/AiController/AiController.js
+++ src/Controllers/AiController/AiController.js
@@ -1,8 +1,13 @@
 import RunAi from "../../Helpers/AiHelper/ai-helper.js";
 
-
-export const RunAiController = async (req,res)=>{
-    const {message} = req.body;
-    let response = await RunAi(message).then(console.log).catch(console.error);
-    res.json(response);
-}
+export const RunAiController = async (req, res) => {
+    const { message } = req.body;
+    try {
+        const response = await RunAi(message);
+        console.log(response);
+        res.json(response[0].message);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
